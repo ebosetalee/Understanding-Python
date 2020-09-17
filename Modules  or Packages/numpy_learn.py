c08e215d8a262186996b97605bc52a9626f4cfdc
ebosetalee <princess01ella.miley@gmail.com>
2020-09-17 14:29:50 +0100

    data science

---
+++ Modules  or Packages/numpy_learn.py
@@ -0,0 +1,16 @@
+# Used for Data Science.
+
+weight_kg = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
+
+import numpy as np
+
+
+# Create a numpy array np_weight_kg from weight_kg
+
+np_weight = np.array(weight)
+
+# Create np_weight_lbs from np_weight_kg
+np_weight_lbs = np_weight * 2.2
+# Print out np_weight_lbs
+print(np_weight_lps)
