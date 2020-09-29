43b67c23f90306c97eb3e456bf669bee05621924
ebosetalee <princess01ella.miley@gmail.com>
2020-09-29 17:25:24 +0100

    added color

--- Modules  or Packages/turtle-module.py
+++ Modules  or Packages/turtle-module.py
@@ -1,9 +1,15 @@
 import turtle
-import time
+# import time
 
 
+# noinspection PyUnresolvedReferences
+# This gets rid of warning messages.
 turtle.forward(150)
 turtle.right(250)
 turtle.forward(150)
 
-time.sleep(5)
+turtle.color("blue")
+turtle.circle(85)
+
+# time.sleep(5)
+turtle.done()
