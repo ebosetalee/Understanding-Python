02d9ffe43cf2455e26b77074c2f5bfb43b5f90c0
ebosetalee <princess01ella.miley@gmail.com>
2020-10-23 02:23:02 +0100

    use of glob

--- Modules  or Packages/date-time-module.py
+++ Modules  or Packages/date-time-module.py
@@ -31,3 +31,14 @@ for i in range(5):
 print(empty_list)
 for i in empty_list:
   print(i)
+
+
+import glob
+import datetime
+
+filenames = glob.glob("*.txt")
+
+with open(str(datetime.datetime.now())+".txt", 'w') as file:
+  for filename1 in filenames:
+    with open(filename1, "r") as f:
+      file.write(f.read()+"\n")
