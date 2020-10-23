7ac7d28b43977c796f65957e0e9238cdceebf7ff
ebosetalee <princess01ella.miley@gmail.com>
2020-10-23 18:42:26 +0100

    initial commit

---
+++ data_structure/circular_queue/circular_queue.py
@@ -0,0 +1,44 @@
+from linked_list.linked_list import LinkedList
+
+
+class CircularQueue:
+    """
+    The CircularQueue uses a LinkedList internally to manage the items
+    """
+
+    def __init__(self):
+        """
+        Initialize the Queue with an empty LinkedList
+        """
+        self.items = LinkedList()
+
+    def is_empty(self):
+        """
+        :returns: True if the LinkedList is empty and False if it isn't
+        """
+        return self.items.head is None
+
+    def enqueue(self, value):
+        """
+        Adds the value to the back of the LinkedList
+        :param value: the value to be added
+        """
+        self.items.add(value)
+        self.items.tail.next = self.items.head
+
+    def dequeue(self):
+        """
+        Removes the value at the head
+        :return: the value of the item at the head
+        """
+        current_node = self.items.head
+        if not self.items.head:
+            return None
+        self.items.head = self.items.head.next
+        return current_node.value
+
+    def peek(self):
+        """
+        :returns: the value of the item at the head
+        """
+        return self.items.head.value
